8ec9cd85bca557adf177540d51627174cc001f3f
CoupCritik <68030927+CoupCritik@users.noreply.github.com>
2023-08-21 14:19:54 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,3 +2,13 @@ class Vector2:
 	def __init__(self, x: float, y: float) -> None:
 		self.x = x
 		self.y = y
+	def __str__(self):
+		return f'({self.x},{self.y})'
+
+class Vector2i:
+	def __init__(self, x: int, y: int) -> None:
+		assert type(x)==int and type(y)==int,"Les paramètres ne sont pas entiers"
+		self.x = x
+		self.y = y
+	def __str__(self):
+		return f'({self.x},{self.y})'
